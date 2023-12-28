f530cdd43e016b3fc21e5fe3c5a2c7a86d6e8a26
qihua <3220497145@qq.com>
2023-12-28 09:08:01 +0800

    前端修改

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target:'http://localhost:10010',//服务器地址
+        target:'http://iw76756254.yicp.fun:45794',//服务器地址
         //生成本地虚拟服务器代理，服务器之间不存在跨域问题
         changeOrigin:true,
         pathRewrite:{
@@ -54,25 +54,38 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
+    proxyTable: {
+      '/api':{
+        target:'http://iw76756254.yicp.fun:45794',//服务器地址
+        //生成本地虚拟服务器代理，服务器之间不存在跨域问题
+        changeOrigin:true,
+        pathRewrite:{
+          '^/api':'' //重新路径，将api替换为空
+        }
+      }
+    },
+
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 9090, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map',
 
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
 
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    cssSourceMap: true
   }
 }
